419170f558466d1f62fce43143e977d167ddef8a
Shaojung <shaojung@gmail.com>
2016-10-24 11:33:38 +0800

    Spinner to change TextView

--- app/src/main/java/com/example/user/yvd102403/MainActivity.java
+++ app/src/main/java/com/example/user/yvd102403/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.user.yvd102403;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        tv = (TextView) findViewById(R.id.textView);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView itemTv = (TextView) view;
+                String str = itemTv.getText().toString();
+                tv.setText(str);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
